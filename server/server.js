4426821190ad2989a0fc273d4c67a5b612411f91
WhiteRain3 <sadykovas.danielius@gmail.com>
2026-01-03 21:02:27 +0200

    splat?

--- server/server.js
+++ server/server.js
@@ -147,7 +147,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   // PATAISYTA: Naudojame :any* parametrą
-  app.get('/:any*', (req, res) => {
+  app.get('/*splat', (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 }
